270c85ee094a146521884083570cf0436c2d0b30
Shibi <shibisuriya.work@gmail.com>
2024-06-06 17:30:48 +0530

    updated image

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,7 +14,7 @@ const Header = () => {
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
             <div className="mr-3">
-              <Image alt={'logo-cat'} src={Logo} width={150}></Image>
+              <Image alt={'logo-cat'} src={Logo} width={80}></Image>
             </div>
             {typeof siteMetadata.headerTitle === 'string' ? (
               <div className="hidden h-6 text-2xl font-semibold sm:block">
