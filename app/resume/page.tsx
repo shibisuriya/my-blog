8339e07effaf6fcb1b267c4502d4e22294afe59c
Shibi <shibisuriya.work@gmail.com>
2024-11-11 23:29:48 +0530

    fix: updated the blog

---
+++ app/resume/page.tsx
@@ -0,0 +1,7 @@
+import { genPageMetadata } from 'app/seo'
+
+export const metadata = genPageMetadata({ title: 'Resume' })
+
+export default function Resume() {
+  return <h1>Resume</h1>
+}
--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -3,6 +3,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
+  { href: '/resume', title: 'Resume' },
 ]
 
 export default headerNavLinks
