58143671bd4aa883f795dc0b733e23ff281f3e8b
Shibi <shibisuriya.work@gmail.com>
2024-11-15 00:19:15 +0530

    fix: updated resume loading

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,10 +1,10 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
+  { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
   { href: '/resume', title: 'Resume' },
-  { href: '/tags', title: 'Tags' },
 ]
 
 export default headerNavLinks
