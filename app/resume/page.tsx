e2520382b07f2f0d018087a5d24046824efcf9cb
Shibi <shibisuriya.work@gmail.com>
2024-11-15 00:12:31 +0530

    fix: added a page for resume

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,7 +1,21 @@
+import { Resume, allResumes } from 'contentlayer/generated'
+import { MDXLayoutRenderer } from 'pliny/mdx-components'
+import AuthorLayout from '@/layouts/AuthorLayout'
+import { coreContent } from 'pliny/utils/contentlayer'
 import { genPageMetadata } from 'app/seo'
+import ResumeLayout from '@/layouts/ResumeLayout'
 
-export const metadata = genPageMetadata({ title: 'Resume' })
+export const metadata = genPageMetadata({ title: 'About' })
 
-export default function Resume() {
-  return <h1>Resume</h1>
+export default function Page() {
+  const [resume]: Resume[] = allResumes
+  const mainContent = coreContent(resume)
+
+  return (
+    <>
+      <ResumeLayout content={mainContent}>
+        <MDXLayoutRenderer code={resume.body.code} />
+      </ResumeLayout>
+    </>
+  )
 }
