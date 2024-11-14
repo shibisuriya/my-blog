2444646881743c3e04e9dcf780cb694d1b0eccc2
Shibi <shibisuriya.work@gmail.com>
2024-11-15 00:31:41 +0530

    project not deploying fiexed

--- layouts/ResumeLayout.tsx
+++ layouts/ResumeLayout.tsx
@@ -1,16 +1,12 @@
 import { ReactNode } from 'react'
-import type { Authors } from 'contentlayer/generated'
-import SocialIcon from '@/components/social-icons'
-import Image from '@/components/Image'
+import type { Resume } from 'contentlayer/generated'
 
 interface Props {
   children: ReactNode
-  content: Omit<Authors, '_id' | '_raw' | 'body'>
+  content: Omit<Resume, '_id' | '_raw' | 'body'>
 }
 
 export default function ResumeLayout({ children, content }: Props) {
-  const { name, avatar, occupation, company, email, twitter, linkedin, github } = content
-
   return (
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
