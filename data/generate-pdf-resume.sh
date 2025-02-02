020a58f41fd7d47d09dabdc821380f29bec5ca12
Shibi <shibisuriya.work@gmail.com>
2025-02-02 16:36:08 +0530

    fix: updated resume

---
+++ data/generate-pdf-resume.sh
@@ -0,0 +1,8 @@
+pandoc resume.mdx -o output.pdf --pdf-engine=xelatex \
+  -V geometry:top=1in \
+  -V geometry:bottom=1in \
+  -V geometry:left=1in \
+  -V geometry:right=1in \
+  -V mainfont="Arial" \
+  -V colorlinks
+
